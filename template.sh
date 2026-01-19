4a140d1bb656a529603bf32b4179c893999989b3
nivedithanmurthy <nivedithanmurthy7@gmail.com>
2026-01-18 21:52:28 -0500

    folder structure created

---
+++ template.sh
@@ -0,0 +1,16 @@
+# Directory
+mkdir -p src
+mkdir -p research
+
+# Files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch app.py
+touch setup.py
+touch research/trails.ipynb
+touch requirements.txt
+
+echo "Directory and files created successfuly."
+ 
